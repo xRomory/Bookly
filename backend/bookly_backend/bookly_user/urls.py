ab804d6c93300ef0eabff81bd3fcbda0331624f5
NicxSummer <pagkatipunannb@students.national-u.edu.ph>
2025-06-11 22:59:15 +0800

    Update urls.py

--- backend/bookly_backend/bookly_user/urls.py
+++ backend/bookly_backend/bookly_user/urls.py
@@ -1,13 +1,29 @@
 from django.urls import path
-from .views import RegisterView, Login, Logout, get_csrf, register_user, csrf_token_view, get_me
+from .views import (
+    RegisterView,
+    Login,
+    Logout,
+    get_csrf,
+    register_user,
+    csrf_token_view,
+    get_me,
+    UpdateProfileView,
+)
 
 urlpatterns = [
+    # CSRF
     path('csrf/', get_csrf, name='get_csrf'),
     path('csrf/token/', csrf_token_view, name="csrf_token"),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('register-user/', register_user, name='register_user'),
-    path('register-cbv/', RegisterView.as_view(), name='register_cbv'),
-    path('me/', get_me, name='get_me'),
+
+    # Registration
+    path('register/', RegisterView.as_view(), name='register_cbv'),
+    path('register-user/', register_user, name='register_user'),  # function-based version
+
+    # Auth
     path('auth/login/', Login.as_view(), name='login'),
     path('auth/logout/', Logout.as_view(), name='logout'),
+
+    # User Profile
+    path('me/', get_me, name='get_me'),
+    path('me/update/', UpdateProfileView.as_view(), name='update_profile'),
 ]
