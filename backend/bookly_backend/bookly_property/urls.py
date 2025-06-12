7bf30ae472525efa79cd8fdff998e7387551fc94
NicxSummer <pagkatipunannb@students.national-u.edu.ph>
2025-06-12 16:20:23 +0800

    Update urls.py

--- backend/bookly_backend/bookly_property/urls.py
+++ backend/bookly_backend/bookly_property/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import BooklyPropertyList, BooklyPropertyCreateView, get_property
-urlpatterns = [
+urlpatterns += [
     path('property-list/', get_property, name='property_details'),
     path('property-list-approved/', BooklyPropertyList.as_view(), name='approved_property_list'),
     path('property-create/', BooklyPropertyCreateView.as_view(), name='property_create'),
