84d78380aaab8651ebc098fa9dbc20f110ffb24c
NicxSummer <pagkatipunannb@students.national-u.edu.ph>
2025-06-12 16:09:18 +0800

    Update urls.py

--- backend/bookly_backend/bookly_property/urls.py
+++ backend/bookly_backend/bookly_property/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('property-list/', get_property, name='property_details'),
     path('property-list-approved/', BooklyPropertyList.as_view(), name='approved_property_list'),
     path('property-create/', BooklyPropertyCreateView.as_view(), name='property_create'),
+    path('has-property/', user_has_properties, name="has_property"),
 ]
