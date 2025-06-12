78a89f7ddaba33a73e5e223ec2ebaaf5829af5d5
NicxSummer <pagkatipunannb@students.national-u.edu.ph>
2025-06-12 15:23:48 +0800

    Update urls.py

--- backend/bookly_backend/bookly_rooms/urls.py
+++ backend/bookly_backend/bookly_rooms/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import BooklyRoomList, BooklyRoomDetailView, api_root, get_rooms
+from .views import BooklyRoomCreateView
 
 urlpatterns = [
     path('api-root/', api_root, name='api_room_root'),
     path('room-list/', BooklyRoomList.as_view(), name='room_list'),
     path('room-detail/', get_rooms, name="room_details"),
     path('room-detail/<int:room_id>/', BooklyRoomDetailView.as_view(), name="room_details"),
+    path('room-create/', BooklyRoomCreateView.as_view(), name="room_create"),
 ]
