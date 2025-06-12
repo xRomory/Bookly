5b61c4a4810ed108d518644c65382cbf275081bf
NicxSummer <pagkatipunannb@students.national-u.edu.ph>
2025-06-12 16:30:14 +0800

    Update urls.py

--- backend/bookly_backend/bookly_rooms/urls.py
+++ backend/bookly_backend/bookly_rooms/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from .views import BooklyRoomList, BooklyRoomDetailView, api_root, get_rooms
-from .views import BooklyRoomCreateView
-from .views import RoomImageUploadView
+from .views import (
+    BooklyRoomList,
+    BooklyRoomDetailView,
+    BooklyRoomCreateView,
+    RoomImageUploadView,
+    get_rooms,
+    api_root,
+    my_property_listing,
+)
 
-urlpatterns += [
+urlpatterns = [
     path('api-root/', api_root, name='api_room_root'),
     path('room-list/', BooklyRoomList.as_view(), name='room_list'),
-    path('room-detail/', get_rooms, name="room_details"),
-    path('room-detail/<int:room_id>/', BooklyRoomDetailView.as_view(), name="room_details"),
-    path('room-create/', BooklyRoomCreateView.as_view(), name="room_create"),
-    path('room-image-upload/', RoomImageUploadView.as_view(), name="room_image_upload"),
-    
+    path('room-detail/', get_rooms, name='room_details'),
+    path('room-detail/<int:room_id>/', BooklyRoomDetailView.as_view(), name='room_details'),
+    path('room-create/', BooklyRoomCreateView.as_view(), name='room_create'),
+    path('room-image-upload/', RoomImageUploadView.as_view(), name='room_image_upload'),
+    path('my-listing/', my_property_listing, name='my_property_listing'),
 ]
